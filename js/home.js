c335809bb11b2cb876890fb3d801d0ee757db4af
ZPPD <zhana.p.d@gmail.com>
2018-11-05 13:34:31 -0500

    fetching and displaying movie and tv data on home page

--- js/home.js
+++ js/home.js
@@ -10,18 +10,18 @@ const table = function(){
     </div>
     <div class='grid-table-bottom'>
         <div class='table-flex'>
-            <button name='getHome' type='button' class='btns active'>Now Playing</button>
-            <button name='getHome' type='button' >Popular</button>
-            <button name='getHome' type='button' >Upcoming</button>
-            <button name='getHome' type='button' >Latest</button>
-            <button name='getHome' type='button'>Top Rated</button>
+            <button name='getHome' type='button' class='btns active' data-get='now_playing' data-type='movie'>Now Playing</button>
+            <button name='getHome' type='button' class='btns' data-get='popular' data-type='movie'>Popular</button>
+            <button name='getHome' type='button' class='btns' data-get='upcoming' data-type='movie'>Upcoming</button>
+            <button name='getHome' type='button' class='btns' data-get='latest' data-type='movie'>Latest</button>
+            <button name='getHome' type='button'class='btns' data-get='top_rated'data-type='movie'>Top Rated</button>
         </div>
         <div class='table-flex'>
-            <button name='getHome' type='button' >Airing Today</button>
-            <button name='getHome' type='button' >Popular</button>
-            <button name='getHome' type='button' >On The Air</button>
-            <button name='getHome' type='button' >Latest</button>
-            <button name='getHome' type='button' >Top Rated</button>
+            <button name='getHome' type='button' class='btns' data-get='airing_today' data-type='tv'>Airing Today</button>
+            <button name='getHome' type='button' class='btns' data-get='popular' data-type='tv'>Popular</button>
+            <button name='getHome' type='button' class='btns' data-get='on_air'data-type='tv'>On The Air</button>
+            <button name='getHome' type='button' class='btns' data-get='latest' data-type='tv'>Latest</button>
+            <button name='getHome' type='button' class='btns' data-get='top_rated'data-type='tv'>Top Rated</button>
         </div>
     </div>
     `; 
@@ -37,51 +37,112 @@ const table = function(){
 
 
 
-//display movies grid
-const displayHome = function(){
-    // API
-    const apiKey = 'b952b137c8f2368ab0069e05f47729a0';
+// //display movies grid
+// const displayHome = function(){
+//     // API
+//     const apiKey = 'b952b137c8f2368ab0069e05f47729a0';
 
-    fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=en-US&page=1`)
-    .then(res => res.json())
-    .then(data => showHomePage(data.results))
-    .catch(err => console.log(err));
-}
+//     fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=en-US&page=1`)
+//     .then(res => res.json())
+//     .then(data => showHomePage(data.results))
+//     .catch(err => console.log(err));
+// }
 
-function showHomePage(movies){
-     //make playing div
-     const playing = document.createElement('div');
-     playing.className = 'playing'; 
-     //get home page
-     const homePage = document.querySelector('.home-page');
-     //get mian
-     const main = document.querySelector('.main');
-      //create an h2  
-      const header = document.createElement('h2');
-      header.className = 'nowPlaying';
-      header.innerHTML = `Now Playing`;
-      homePage.appendChild(header);
+// function showHomePage(movies){
+//      //make playing div
+//      const playing = document.createElement('div');
+//      playing.className = 'playing'; 
+//      //get home page
+//      const homePage = document.querySelector('.home-page');
+//      //get mian
+//      const main = document.querySelector('.main');
+//       //create an h2  
+//       const header = document.createElement('h2');
+//       header.className = 'nowPlaying';
+//       header.innerHTML = `Now Playing`;
+//       homePage.appendChild(header);
 
-     //loop through cast
-     movies.forEach(movie => {
+//      //loop through movies
+//      movies.forEach(movie => {
          
-         console.log(movie);
+//          console.log(movie);
         
-         //make gridMovie div
-         const gridMovie = document.createElement('div');
-         gridMovie.className = 'playing-grid';
-         const type = 'movie';
-         //fill in actors div 
-         gridMovie.innerHTML = `
-             <div class='playing-card' onClick='movieSelected("${movie.id}", "${type}")'>
-                 <img class='playing-img' src='https://image.tmdb.org/t/p/original${movie.poster_path}' alt="Now Playing movies">
-                 <h3 class='playingMovie-name'>${movie.title}</h3> 
-             </div>       
-     `;
-         playing.appendChild(gridMovie);  
-         homePage.appendChild(playing);
-     });       
+//          //make gridMovie div
+//          const gridMovie = document.createElement('div');
+//          gridMovie.className = 'playing-grid';
+//          const type = 'movie';
+//          //fill in actors div 
+//          gridMovie.innerHTML = `
+//              <div class='playing-card' onClick='movieSelected("${movie.id}", "${type}")'>
+//                  <img class='playing-img' src='https://image.tmdb.org/t/p/original${movie.poster_path}' alt="Now Playing movies">
+//                  <h3 class='playingMovie-name'>${movie.title}</h3> 
+//              </div>       
+//      `;
+//          playing.appendChild(gridMovie);  
+//          homePage.appendChild(playing);
+//      });       
+// }
+//fetch info from buttons
+const fetchInfo = function(){
+    const btns = document.querySelectorAll('.btns');
+    btns.forEach(btn => btn.addEventListener('click',fetchBtnData));
+    function fetchBtnData (e){
+        const type = e.target.dataset.type;
+        console.log(type);
+        const getData = e.target.dataset.get;
+        console.log(getData);
+        
+        // API
+        const apiKey = 'b952b137c8f2368ab0069e05f47729a0';
+
+            fetch(`https://api.themoviedb.org/3/${type}/${getData}?api_key=${apiKey}&language=en-US&page=1`)
+            .then(res => res.json())
+            .then(data => showDataHome(data.results))
+            .catch(err => console.log(err));
+            
+    //show data from request on buttons
+    function showDataHome(movies){
+        //make playing div
+        const playing = document.createElement('div');
+        playing.className = 'playing'; 
+        
+        //get home page
+        const homePage = document.querySelector('.home-page');   
+        
+        
+        //loop through movies
+        movies.forEach(movie => {
+            console.log(movie);
+            // const types = this.dataset.type;
+            console.log(type);
+            //make gridMovie div
+            const gridMovie = document.createElement('div');
+            gridMovie.className = 'playing-grid';
+            
+            //fill in homepage div 
+            if(type === 'movie' && e.target.classList.contains('active')){
+                gridMovie.innerHTML = `
+                <div class='playing-card' onClick='movieSelected("${movie.id}", "${type}")'>
+                    <img class='playing-img' src='https://image.tmdb.org/t/p/original${movie.poster_path}' alt="Now Playing movies">
+                    <h3 class='playingMovie-name'>${movie.title}</h3> 
+                </div>       
+        `;}else if(type === 'tv' && e.target.classList.contains('active')){
+            gridMovie.innerHTML = `
+            <div class='playing-card' onClick='movieSelected("${movie.id}", "${type}")'>
+                <img class='playing-img' src='https://image.tmdb.org/t/p/original${movie.poster_path}' alt="Now Playing movies">
+                <h3 class='playingMovie-name'>${movie.name}</h3> 
+            </div>       
+        `;
+            }
+            playing.innerHTML = '';
+            
+            playing.appendChild(gridMovie);  
+            homePage.appendChild(playing);
+        });       
+   }
+}     
 }
+
 // select a movie and get details
 function movieSelected(id, type){
     sessionStorage.setItem('movieId', id);
@@ -98,5 +159,6 @@ document.addEventListener("DOMContentLoaded", init);
 function init() { 
   //init homepage
   new table();
-  new displayHome();
+//   new displayHome();
+  new fetchInfo();
 }
